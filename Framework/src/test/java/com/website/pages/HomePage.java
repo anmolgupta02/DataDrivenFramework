c16eef5deb8023922bc79cbe587d0b8c2185f79a
appventurez <appventurez@appventurez-HP-ProBook-440-G1>
2019-07-22 16:52:10 +0530

    Created page for signUp and formatted data in HomePage class

--- Framework/src/test/java/com/website/pages/HomePage.java
+++ Framework/src/test/java/com/website/pages/HomePage.java
@@ -13,10 +13,21 @@ public class HomePage {
 		this.driver = driver;
 	}
 	
-	@FindBy(xpath = "/html/body/app-root/app-theme/div/div/div[1]/app-header/div[1]/nav/div/ul/li[7]/a")WebElement loginButtonTrigger;
-	@FindBy(xpath = "/html/body/ngb-modal-window/div/div/form/div[2]/div/div/div/div[2]/input")WebElement emailField;
-	@FindBy(xpath = "/html/body/ngb-modal-window/div/div/form/div[2]/div/div/div/div[3]/input")WebElement passField;
-	@FindBy(xpath = "/html/body/ngb-modal-window/div/div/form/div[2]/div/div/div/div[4]/button")WebElement LoginButton;
+	@FindBy
+	(xpath = "/html/body/app-root/app-theme/div/div/div[1]/app-header/div[1]/nav/div/ul/li[7]/a")
+	WebElement loginButtonTrigger;
+	
+	@FindBy
+	(xpath = "/html/body/ngb-modal-window/div/div/form/div[2]/div/div/div/div[2]/input")
+	WebElement emailField;
+	
+	@FindBy
+	(xpath = "/html/body/ngb-modal-window/div/div/form/div[2]/div/div/div/div[3]/input")
+	WebElement passField;
+	
+	@FindBy
+	(xpath = "/html/body/ngb-modal-window/div/div/form/div[2]/div/div/div/div[4]/button")
+	WebElement LoginButton;
 	
 	
 	public void LoginProcess(String email, String Password) throws Exception {
---
+++ Framework/src/test/java/com/website/pages/SignUpPage.java
@@ -0,0 +1,22 @@
+package com.website.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SignUpPage {
+	
+	WebDriver driver;
+
+	public SignUpPage(WebDriver driver){
+		
+		this.driver = driver; 
+	}
+	
+	@FindBy
+	(xpath = "")
+	WebElement FirstName;
+	
+	
+	
+}
